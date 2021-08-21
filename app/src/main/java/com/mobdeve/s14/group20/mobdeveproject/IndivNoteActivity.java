97edb60335c68b14ea8b0e14789372e506f443f0
LanderPeterCua <80734810+LanderPeterCua@users.noreply.github.com>
2021-08-21 23:16:22 +0800

    created xml for main activity and indiv note activity

--- app/src/main/java/com/mobdeve/s14/group20/mobdeveproject/IndivNoteActivity.java
+++ app/src/main/java/com/mobdeve/s14/group20/mobdeveproject/IndivNoteActivity.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class IndivNoteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indiv_note);
-
-        int integer1, integer2, integer3;
     }
 }
