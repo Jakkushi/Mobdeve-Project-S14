0b50b14119187ec51b9d04901e8fc3a099d7190e
LanderPeterCua <80734810+LanderPeterCua@users.noreply.github.com>
2021-08-21 16:42:12 +0800

    TEST 2

--- app/src/main/java/com/mobdeve/s14/group20/mobdeveproject/MainActivity.java
+++ app/src/main/java/com/mobdeve/s14/group20/mobdeveproject/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int integer1;
+        int integer1, integer2;
     }
 }
