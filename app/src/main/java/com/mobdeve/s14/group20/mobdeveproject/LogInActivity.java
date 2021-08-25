0be8f3b9fe5c5aa36f2c0eb1779d3c2e24e25b6c
LanderPeterCua <80734810+LanderPeterCua@users.noreply.github.com>
2021-08-25 15:05:56 +0800

    Added log-in view and activity

---
+++ app/src/main/java/com/mobdeve/s14/group20/mobdeveproject/LogInActivity.java
@@ -0,0 +1,22 @@
+package com.mobdeve.s14.group20.mobdeveproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LogInActivity extends AppCompatActivity {
+
+    EditText etEmail, etPassword;
+    Button btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+
+
+}
